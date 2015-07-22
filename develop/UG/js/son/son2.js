67db038e581d5845a9275ed60903b5a13b6e5789
郭朋飞 <guopengfei@wxldeMac-mini.local>
2015-07-22 18:44:04 +0800

    重组测试数据，add clean

--- develop/UG/js/son/son2.js
+++ develop/UG/js/son/son2.js
@@ -1,1 +1,3 @@
 var son2 = 'son2';
+son2 = new Date().getDate % 2;
+console.log(son2);
--- grunt_task_cfg/clean.js
+++ grunt_task_cfg/clean.js
@@ -1,7 +1,7 @@
+exports.cleanDir = [];
 
 //调试模式下清除
 if(gruntProject.debug) {
-    exports.cleanDir = [];
     exports.cleanDir.push(gruntProject.dest);
 }
 
