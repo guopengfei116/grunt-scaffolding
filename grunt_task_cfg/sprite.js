d2d549c466aadf9fc6101d51022e471bc6c55ec1
郭朋飞 <guopengfei@pengfei.local>
2015-08-07 16:57:38 +0800

    modify config

--- grunt_task_cfg/sprite.js
+++ grunt_task_cfg/sprite.js
@@ -3,5 +3,4 @@ var buildConfig = require('../lib/buildConfig')(gruntProject.src, 'img');
 
 var spriteConfig = buildConfig.initBuildDir().build();
 
-console.log(spriteConfig);
 module.exports = spriteConfig;
